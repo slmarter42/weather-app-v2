c8c18c0edb900d7260b3d102cdaa0dae1085dd40
slmarter42 <slmarter42@tntech.edu>
2025-02-24 15:39:22 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,43 +5,34 @@ const urlsToCache = [
   "/",
   "/index.html",
   "/manifest.json",
-  "/images/favicon.ico",
-  "/images/apple-touch-icon-iphone60x60.png",
-  "/images/apple-touch-icon-ipad-76x76.png",
-  "/images/apple-touch-icon-iphone-retina-120x120.png",
-  "/images/apple-touch-icon-ipad-retina-152x152.png",
+  <link rel="icon" type="image/x-icon" href="/images/favicon.ico"> 
+    <link rel="apple-touch-icon" href="images/apple-touch-icon-iphone60x60.png"> 
+    <link rel="apple-touch-icon" sizes="60x60" href="images/apple-touch-icon-ipad-76x76.png"> 
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-iphone-retina-120x120.png"> 
+    <link rel="apple-touch-icon" sizes="144x144" href="images/apple-touch-icon-ipad-retina-152x152.png">
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css"
 ];
-
-// Install event: cache all required assets
-self.addEventListener("install", (event) => {
+//Install Event
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Opened cache");
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Fetch event: serve cached assets when offline
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      // Return cached response if found, otherwise fetch from network
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
 
-// Activate event: remove old caches
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then((cacheNames) =>
+    caches.keys().then(cacheNames =>
       Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
             return caches.delete(cacheName);
           }
         })
